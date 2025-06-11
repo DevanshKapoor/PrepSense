e0bc7dd209fa0bd5b799133f18dde9e80964bb05
DevanshKapoor <kapoordevansh368@gmail.com>
2025-06-12 02:15:47 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Dashboard from "./Dashboard";
 import LandingPage from "./LandingPage";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
